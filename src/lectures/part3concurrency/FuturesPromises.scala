ac8a81c582d178ae31887f44f107fb936a391f50
Ștefan Ignătescu <ignatescustefan@gmail.com>
2022-03-28 17:26:09 +0300

    29. Futures, Part 3

--- src/lectures/part3concurrency/FuturesPromises.scala
+++ src/lectures/part3concurrency/FuturesPromises.scala
@@ -1,7 +1,8 @@
 package lectures.part3concurrency
 
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.Future
+import scala.concurrent.duration._
+import scala.concurrent.{Await, Future, Promise}
 import scala.util.{Failure, Random, Success}
 
 object FuturesPromises extends App {
@@ -102,4 +103,62 @@ object FuturesPromises extends App {
 
   val fallbackResult = SocialNetwork.fetchProfile("unknown id").fallbackTo(SocialNetwork.fetchProfile("fb.id.0-dummy"))
 
+
+  // online banking app
+
+  case class User(name: String)
+
+  case class Transaction(sender: String, receiver: String, amount: Double, status: String)
+
+  object BankingApp {
+    val name = "Rock the JVM banking"
+
+    def fetchUser(name: String): Future[User] = Future {
+      // simulate
+      Thread.sleep(500)
+      User(name)
+    }
+
+    def createTransaction(user: User, merchantName: String, amount: Double): Future[Transaction] = Future {
+      // simulate
+      Thread.sleep(1000)
+      Transaction(user.name, merchantName, amount, "Success")
+    }
+
+    def purchase(username: String, item: String, merchantName: String, cost: Double): String = {
+      // fetch the user from db
+      // create a transaction
+      // wait for the transaction
+      val transactionStatusFuture = for {
+        user <- fetchUser(username)
+        transaction <- createTransaction(user, merchantName, cost)
+      } yield transaction.status
+
+      Await.result(transactionStatusFuture, 2.seconds) // implicit conversions -> pimp my library
+    }
+  }
+
+  println(BankingApp.purchase("Stefan", "apple", "store", 100))
+
+  // promises
+
+  val promise = Promise[Int]() // "controller" over a future
+
+  val future = promise.future
+
+  // thread 1 - "consumer"
+
+  future.onComplete {
+    case Success(r) => println(s"[consumer] I've received $r")
+  }
+
+  val producer = new Thread(() => {
+    println("[producer] crunching numbers..")
+    Thread.sleep(1000)
+    promise.success(42)
+    println("[producer] done")
+  })
+
+  producer.start()
+  Thread.sleep(2000)
 }
